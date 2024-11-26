5a1392358844d5e4040c0e86a2fd5e1eab4dcd36
deveshyadav3 <deveshyadavya@gmail.com>
2024-11-26 13:05:39 +0000

    contaner config done

---
+++ 01_basics/01_pad.js
@@ -0,0 +1,15 @@
+const accountid=144553;
+let accountemail="deveshyadavya@gmail.com";
+var accountpassward="12345";
+let accountcity= "india";
+
+//accountid = 2 // not allowed
+
+accountemail="deveshyadavabc@gmail.com";
+accountpassward="13121";
+accountcity="japan";
+let accounntstate;
+
+console.log(accountid);
+
+console.table([accountid,accountcity,accountemail,accountpassward,accounntstate]);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # js-hindi-youtupe
 a code repo for java script 
+/workspaces/js-hindi-youtupe/01_basics
